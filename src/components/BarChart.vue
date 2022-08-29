64a37767748e37c3a184847beeb3b39e1bd8d6a5
Mark Gudorf <gudorfm@gmail.com>
2022-08-29 16:50:15 -0400

    Example chart working, vue-chartjs dependencies worked out

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -6,7 +6,7 @@
 </template>
   
   <script>
-  import { Bar } from 'vue-chartjs'
+  import  { Bar } from 'vue-chartjs/legacy'
   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
   
   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
